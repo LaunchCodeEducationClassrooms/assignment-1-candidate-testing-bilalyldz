062bcbba2bc161b9871a94fd4c33e239c7d3fdcc
Bilal Yildiz <bilalyildizusa@gmail.com>
2021-02-17 03:45:26 +0000

    Last part needs to be modified

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,36 +3,63 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName="";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question="Who was the first American woman in space? ";
+let correctAnswer="Sally Ride";
+let candidateAnswer="";
+let questions=["Who was the first American woman in space?","True or false: 5000 meters = 5 kilometers.", "(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
+let correctAnswers=["Sally Ride","True","40", "Trajectory",	"3"];
+let candidateAnswers=[];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName=input.question("Please enter your name dear candidate: ");
+console.log(`Welcome to Space Program Dear Candidate ${candidateName}! `)
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer//
 
 
+
+for(let i=0; i<questions.length; i++){
+
+candidateAnswer=input.question(questions[i]);
+candidateAnswers.push(candidateAnswer);
+console.log("Your answer "+candidateAnswer);
+console.log("Correct answer "+correctAnswers[i]);
+}
+
+console.log(candidateAnswers);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  let count=0;
+for(let i=0; i<questions.length; i++){
+if(candidateAnswers[i]===correctAnswers[i]){
+  count++
+}
+}
 
   let grade;
-  
+  let numberOfQuestions=questions.length;
+  let numberOfCorrectAnswer=count;
+  //(Number of Correct Answers) / (Number of Quiz Questions) * 100
+    grade=(count)/(numberOfQuestions)*100;
+    console.log(grade);
+
 
   return grade;
+  console.log(`>>> Overall Grade: ${grade}% (${count} of 5 responses correct) <<<`);
+  if(grade>=80){
+    console.log(">>> Status: SUCCESSFULL <<<");
+  }else{
+    console.log(">>> Status: FAILED <<<")
+  }
 }
 
 function runProgram() {
