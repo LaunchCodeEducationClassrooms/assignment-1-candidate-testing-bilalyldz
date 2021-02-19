0ba69ba6e343472b2b26b312c2224cb86b08de3c
Bilal Yildiz <bilalyildizusa@gmail.com>
2021-02-19 23:11:27 +0000

    In gradeQuiz method i forgot to place return expression.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName='';
 let question="Who was the first American woman in space? ";
 let correctAnswer="Sally Ride";
 let candidateAnswer="";
-let questions=['Who was the first American woman in space? ','True or false: 5000 meters = 5 kilometers. ', '(5 + 3)/2 * 10 = ?',"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",'What is the minimum crew size for the ISS? '];
+let questions=['Who was the first American woman in space? ','True or false: 5000 meters == 5 kilometers. ', '(5 + 3)/2 * 10 = ? ',"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",'What is the minimum crew size for the ISS? '];
 let correctAnswers=['Sally Ride','true','40','Trajectory','3'];
 let candidateAnswers=[];
 
@@ -50,16 +50,19 @@ if((candidateAnswers[i].toLowerCase())===(correctAnswers[i].toLowerCase())){
   let numberOfCorrectAnswer=count;
   //(Number of Correct Answers) / (Number of Quiz Questions) * 100
     grade=(count)/(numberOfQuestions)*100;
-    console.log(grade);
+   // console.log(grade);
 
   
   if(grade>=80){
     console.log(`>>> Overall Grade: ${grade}% (${count} of 5 responses correct) <<< \n>>> Status: SUCCESSFULL <<<;`);
+     return grade;
     
   }else{
      console.log(`>>> Overall Grade: ${grade}% (${count} of 5 responses correct) <<< \n>>> Status: FAILED <<<`);
+      return grade;
     
   }
+ 
 }
 
 function runProgram() {
